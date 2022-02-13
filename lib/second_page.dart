9a42dec299c68a26456f882f21bc6b79c1465cd8
Benedicta Frema Boamah <bfboamah9@gmail.com|>
2022-02-13 22:02:45 +0000

    feat:created second page

---
+++ lib/second_page.dart
@@ -0,0 +1,34 @@
+import 'package:befit/widgets/lower_container.dart';
+import 'package:befit/widgets/upper_container.dart';
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: const [
+            UpperContainer(imageName: 'fitness1.png'),
+            LowerContainer(
+              firstTextList: [
+                'Customize',
+                'Feel free to adjust your',
+                'workout & diet settings',
+                'according to your comfort.',
+                'Next'
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
