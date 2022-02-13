54c28034e30c662e9c3a963633128c75d252b1f9
Malik Kolade <kolademalikademola@gmail.com>
2022-02-13 20:31:45 +0000

    button widget created

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  const Button({ Key? key }) : super(key: key);
+  const Button({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Skip',
+          style: TextStyle(color: Colors.grey),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Next'),
+          style: ElevatedButton.styleFrom(
+              elevation: 0,
+              padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 14),
+              primary: Colors.black,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32))),
+        ),
+      ],
     );
   }
 }
