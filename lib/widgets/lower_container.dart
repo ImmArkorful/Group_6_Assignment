7514bce232bfd2598a768e1d51c748ac39b7cac5
ImmArkorful <e.arkorful3@gmail.com>
2022-02-13 21:56:50 +0000

    feat: first page created

--- lib/widgets/lower_container.dart
+++ lib/widgets/lower_container.dart
@@ -1,22 +1,44 @@
+import 'package:befit/widgets/button.dart';
+import 'package:befit/widgets/progress_bar.dart';
+import 'package:befit/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class LowerContainer extends StatelessWidget {
-  const LowerContainer({ Key? key }) : super(key: key);
+  const LowerContainer({Key? key, required this.firstTextList})
+      : super(key: key);
+
+  final List firstTextList;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-            padding: const EdgeInsets.only(top: 32, left: 32, right: 32),
-            transform: Matrix4.translationValues(0.0, -30.0, 0.0),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              color: Colors.white,
-            ),
-          );
+      padding: const EdgeInsets.only(top: 32, left: 32, right: 32),
+      transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.4,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          TextWidget(textList: firstTextList),
+          const SizedBox(
+            height: 20,
+          ),
+          const ProgressBar(
+            widthList: [24.0, 10.0, 10.0],
+            colors: [Colors.red, Colors.green, Colors.grey],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Button()
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/material.dart';
 
 class ProgressBar extends StatelessWidget {
-  const ProgressBar({ Key? key }) : super(key: key);
+  const ProgressBar({Key? key, required this.widthList, required this.colors})
+      : super(key: key);
+
+  final List widthList;
+  final List colors;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-                  width: 50,
-                  height: 10,
-                  child: Row(
-                    children: [
-                      Expanded(
-                          child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.green,
-                        ),
-                      )),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 3),
-                        width: 10,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.red,
-                        ),
-                      ),
-                      Container(
-                        width: 10,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.blue,
-                        ),
-                      )
-                    ],
-                  ),
-                );
+      width: 50,
+      height: 10,
+      child: Row(
+        children: [
+          Container(
+            width: widthList[0],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: colors[0],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 3),
+            width: widthList[1],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: colors[1],
+            ),
+          ),
+          Container(
+            width: widthList[2],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: colors[2],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
