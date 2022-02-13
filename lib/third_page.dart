e4be72806476a6b882af25fc59640ba1837d8061
Iniubong Arthur <arthurshur@gmail.com>
2022-02-13 22:54:46 +0000

    creating third screen

---
+++ lib/third_page.dart
@@ -0,0 +1,10 @@
+import 'package:befit/widgets/lower_container.dart';
+import 'package:befit/widgets/upper_container.dart';
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatefulWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+
+  @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
