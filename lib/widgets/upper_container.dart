d94e62330d180e94df5c24b92508b62a9eefba51
Wemimo Olapade <wemmy.olapade@gmail.com>
2022-02-13 19:59:58 +0000

    upper container widget

---
+++ lib/widgets/upper_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UpperContainer extends StatelessWidget {
+  const UpperContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
