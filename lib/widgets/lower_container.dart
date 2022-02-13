2ad8340a303299285a516b10b343f85b62f5cca0
Benedicta Frema Boamah <bfboamah9@gmail.com|>
2022-02-13 20:13:10 +0000

    feat:created a container

--- lib/widgets/lower_container.dart
+++ lib/widgets/lower_container.dart
@@ -5,6 +5,18 @@ class LowerContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.only(top: 32, left: 32, right: 32),
+      transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.4,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        color: Colors.white,
+      ),
+    );
   }
 }
