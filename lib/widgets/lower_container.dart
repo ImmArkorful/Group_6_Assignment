fe853ad1589a2a62801d46f0e42d0d84afb05552
Benedicta Frema Boamah <bfboamah9@gmail.com|>
2022-02-13 20:08:44 +0000

    feat:created a lower container widget

---
+++ lib/widgets/lower_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LowerContainer extends StatelessWidget {
+  const LowerContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
